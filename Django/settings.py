490cb49769273296112d214e6f7fd9715930f582
shion <2201376@s.asojuku.ac.jp>
2025-10-15 10:33:44 +0900

    main.auth アプリの AuthConfig を MainAuthConfig に変更し、初期マイグレーションを追加

--- Django/settings.py
+++ Django/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'main.auth.apps.AuthConfig',  
+    'main.auth.apps.MainAuthConfig',  
 ]
 
 MIDDLEWARE = [
