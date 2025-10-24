0c9709f0efef26438facae6b3b8eefcc9d4c6a11
shion <2201376@s.asojuku.ac.jp>
2025-10-24 11:38:07 +0900

    管理者画面７

--- Admin/urls.py
+++ Admin/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import admin_top
 
 urlpatterns = [
-
+    path('top/', admin_top.admin_top, name='admin_top'),
     path('users/', admin_top.user_management, name='admin_users'),
     path('prompts/', admin_top.prompt_management, name='admin_prompts'),
 ]
