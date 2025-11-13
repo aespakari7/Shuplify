c5aa52c94c28f0c8317c4170e8ea40487b7b825e
koshi <2201035@s.asojuku.ac.jp>
2025-11-13 12:27:12 +0900

    修正

--- main/auth/AI_ES.py
+++ main/auth/AI_ES.py
@@ -141,7 +141,7 @@ def aies(request):
 
             # --- モデルの初期化とAPI呼び出し ---
             model = genai.GenerativeModel(
-                model_name="gemini-1.5-flash-latest",
+                model_name="gemini-2.0-flash",
                 generation_config=generation_config,
                 safety_settings=safety_settings,
                 #☆変更
